5a067142f296129c44f901109cdb7129d57c75d2
matsalen <k.kurosaki@hotmail.com>
2019-09-17 17:13:29 -0300

    Exemplo animal com herança

---
+++ ExemploHeranca/Assets/Scripts/Gato.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gato : Animal
+{
+    protected override void Fala()
+    {
+        Debug.Log("Salve, aqui é o " + nome);       
+    }
+}
---
+++ ExemploHeranca/Assets/Scripts/Passaro.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Passaro : Animal
+{
+    protected override void Fala()
+    {
+        Debug.Log("Piu piu");
+    }
+
+    protected override void Pula()
+    {
+        velocidadeVertical = 10f;
+    }
+}
